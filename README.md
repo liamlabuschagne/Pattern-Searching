0822d05b6875c8a8155014facd733e6b9a4e5290
liamlabuschagne <liaml@disroot.org>
2022-04-20 16:07:03 +1200

    Everything works bar alternation.

--- README.md
+++ README.md
@@ -7,28 +7,35 @@ This project aims to allow searching through text using a regular expression pat
 ### Grammar
 The grammar of the regular expression compiler is as follows:
 ```
-E → T
-E → TE
-T → C
-T → C|T
-C → F
-C → F*
-C → F+
-C → F?
+E → A
+E → AE
+A → C
+A → C|A
+C → R
+C → RC
+R → P
+R → P*
+R → P+
+R → P?
+P → X
+P → (X)
+X → F
+X → \n
 F → .
 F → l
-F → \n
-F → (E)
 
 Where:
 
 E is a regular expression
-T is a term
-C is a closure
-F is a factor
-l is a literal
+A is an alternation
+C is concatenation
+R is repetition
+P is parenthesis
+X is an escaped character
 n is a non-literal
+F is a factor
 . is any literal
+l is a literal
 * is 0 or more
 + is 1 or more
 ? is 0 or 1
@@ -43,7 +50,7 @@ The order of precedence is as follows:
 2. parentheses (i.e. the most deeply nested regexps have the highest precedence)
 3. repetition/option operators (i.e. *, + and ?)
 4. concatenation
-5. alternation (i.e. | and [ ]) 
+5. alternation (i.e. | and [ ])
 
 ### Pre-Processing
 The compiler will also convert the following syntax using a pre-processor:
--- REcompile.java
+++ REcompile.java
@@ -1,7 +1,11 @@
 class REcompile {
 
-    private static String r = "";
-    private static int i = 0;
+    private static String r = ""; // The expression we are compiling
+    private static int i = 0; // Pointer into the expression
+    private static int s = 0; // Which state are we building
+    private static char[] ch; // Character of each state in FSM
+    private static int[] n1; // Next 1 of each state in FSM
+    private static int[] n2; // Next 2 of each state in FSM
 
     public static void main(String args[]) {
         if (args.length != 1) {
@@ -9,64 +13,151 @@ class REcompile {
         }
         r = args[0];
         System.out.println("Compiling: " + r);
-        expression();
+
+        // Initialize FSM arrays to be the length of the expression since we need one
+        // state per symbol in the expression except for parenthesis but it is not worth
+        // scanning through to see how many of them there are.
+
+        ch = new char[r.length() * 2];
+        n1 = new int[r.length() * 2];
+        n2 = new int[r.length() * 2];
+
+        // Pad start with single direction branching machine
+        setState(s, (char) 1, s + 1, s + 1);
+        s++;
+
+        System.out.println("FSM starts at state: " + expression());
+        showFSM();
     }
 
-    private static void expression() {
-        term();
-        if (i != r.length() && (isLiteral(r.charAt(i)) || r.charAt(i) == '('))
+    private static int expression() {
+        int start = alternation();
+
+        if (i < r.length() && (isFactor() || r.charAt(i) == '(' || r.charAt(i) == '\\'))
             expression();
+
+        return start;
     }
 
-    private static void term() {
-        closure();
-        if (i == r.length())
-            return;
-        if (r.charAt(i) == '|') {
+    private static int alternation() {
+        int start = concatenation();
+
+        // Optionally do an alternation
+
+        return start;
+    }
+
+    private static int concatenation() {
+        int start = repetition();
+
+        // Optionally concatenates another concatenation
+        if (i < r.length() && (isFactor() || r.charAt(i) == '(' || r.charAt(i) == '\\'))
+            concatenation();
+
+        return start;
+    }
+
+    private static int repetition() {
+        int start = parenthesis();
+        int prev = start - 1;
+
+        // Optionally does a repetition
+        if (i < r.length() && r.charAt(i) == '*') {
             i++;
-            term();
-            return;
+            setState(s, (char) 1, start, s + 1);
+            int b = s;
+            s++;
+            // Update prev machine to point to branching machine
+            setState(prev, ch[prev], b, b);
+
+            // Finish off with single direction branching machine
+            setState(s, (char) 1, s + 1, s + 1);
+            s++;
+        } else if (i < r.length() && r.charAt(i) == '+') {
+            i++;
+            setState(s, (char) 1, start, s + 1);
+            s++;
+
+            // Finish off with single direction branching machine
+            setState(s, (char) 1, s + 1, s + 1);
+            s++;
+        } else if (i < r.length() && r.charAt(i) == '?') {
+            i++;
+            setState(s, (char) 1, start, s + 1);
+            int b = s;
+            s++;
+            // Update prev machine to point to branching machine
+            setState(prev, ch[prev], b, b);
+
+            // Update start to point to next machine
+            setState(start, ch[start], s, s);
+
+            // Finish off with single direction branching machine
+            setState(s, (char) 1, s + 1, s + 1);
+            s++;
         }
+
+        return start;
     }
 
-    private static void closure() {
-        factor();
-        if (i == r.length())
-            return;
-        if (r.charAt(i) == '*' || r.charAt(i) == '+' || r.charAt(i) == '?') {
-            i++;
-            return;
+    private static int parenthesis() {
+        int start = -1;
+        if (r.charAt(i) != '(') {
+            start = escape();
         }
-    }
 
-    private static void factor() {
-
-        if (r.charAt(i) == '(') {
+        // Optionally does a nested expression (E)
+        if (i < r.length() && r.charAt(i) == '(') {
             i++;
-            expression();
-            if (i == r.length() || r.charAt(i) != ')')
+            start = expression();
+            if (i >= r.length() || r.charAt(i) != ')')
                 error();
             i++;
-            return;
         }
 
-        if (isLiteral() || r.charAt(i) == '.') {
+        return start;
+    }
+
+    private static int escape() {
+        int start = -1;
+        if (r.charAt(i) == '\\') {
+            // Handle escaped non-literal
+            i++; // Move past the \
+
+            // Create a machine as if it is a literal
+            setState(s, r.charAt(i), s + 1, s + 1);
+            start = s;
+            s++;
             i++;
-            return;
+        } else if (isLiteral()) {
+            start = factor();
         }
 
-        if (r.charAt(i) == '\\' && !isLiteral(r.charAt(i + 1))) {
-            i += 2;
-            return;
+        return start;
+    }
+
+    private static int factor() {
+        int start = -1;
+        if (isLiteral() || r.charAt(i) == '.') {
+            // Handle literal
+            setState(s, r.charAt(i), s + 1, s + 1);
+            start = s;
+            s++;
+            i++;
         }
 
-        error();
+        return start;
+    }
+
+    private static boolean isFactor() {
+        if (i >= r.length())
+            error();
+        return (isLiteral(r.charAt(i)) || r.charAt(i) == '.');
     }
 
     private static boolean isLiteral() {
-        if (i == r.length()) {
+        if (i >= r.length())
             error();
-        }
         return isLiteral(r.charAt(i));
     }
 
@@ -90,5 +181,38 @@ class REcompile {
         System.out.println(padding + "^");
 
         System.exit(0);
+    }
+
+    private static void setState(int st, char c, int ne1, int ne2) {
+        ch[st] = c;
+        n1[st] = ne1;
+        n2[st] = ne2;
+    }
+
+    private static void showFSM() {
+        System.out.println("s#|ch|n1|n2|");
+        for (int j = 0; j < s; j++) {
+            String stateNumber = j + "";
+            if (j < 10) {
+                stateNumber += " ";
+            }
+
+            String character = ch[j] + " ";
+            if (ch[j] == (char) 1) {
+                character = "  ";
+            }
+
+            String next1 = n1[j] + "";
+            if (next1.length() != 2) {
+                next1 += " ";
+            }
+
+            String next2 = n2[j] + "";
+            if (next2.length() != 2) {
+                next2 += " ";
+            }
+
+            System.out.println(stateNumber + "|" + character + "|" + next1 + "|" + next2 + "|");
+        }
     }
 }
